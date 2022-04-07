83cb217a7c1b276d265a5149bce9536820ea55b7
shinysource <highly.flores@gmail.com>
2022-04-07 21:57:43 +0900

    Added label to FormCheck

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -3,6 +3,7 @@ import * as Yup from 'yup'
 import { Grid } from '@mui/material'
 
 import FormInput from '../../components/Fields/FormInput'
+import FormCheck from '../../components/Fields/FormCheck'
 
 const validationSchema = Yup.object().shape({
   firstName: Yup.string().required('Enter your Firstname'),
@@ -38,58 +39,108 @@ const Signup = () => {
       alignItems="center"
       className="register-back"
     >
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container direction="column" alignItems="center" spacing={2}>
-          <Grid item className="text-white text-[9px]">
-            <div>Already have an account</div>
-          </Grid>
-          <Grid item>
-            <FormInput
-              name="firstName"
-              formik={formik}
-              handleChange={formik.handleChange}
-              className="font-inter font-normal text-base w-[311px]"
-              label="First name"
-              placeholder="First name"
-              isHint={true}
-            />
-          </Grid>
-          <Grid item>
-            <FormInput
-              name="lastName"
-              formik={formik}
-              handleChange={formik.handleChange}
-              className="font-inter font-normal text-base w-[311px]"
-              label="Last name"
-              placeholder="Last name"
-              isHint={true}
-            />
-          </Grid>
-          <Grid item>
-            <FormInput
-              type="email"
-              name="email"
-              formik={formik}
-              handleChange={formik.handleChange}
-              className="font-inter font-normal text-base w-[311px]"
-              label="Email"
-              placeholder="Email"
-              isHint={true}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <FormInput
-              type="string"
-              name="mobile"
-              formik={formik}
-              handleChange={formik.handleChange}
-              label="Mobile(optional)"
-              className="font-inter font-normal text-base w-[311px] "
-              placeholder="Mobile(optional)"
-            />
-          </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item>
+          <img src="public/assets/logo/Logo.svg"></img>
         </Grid>
-      </form>
+        <Grid item className="text-white font-bold text-[32px]">
+          <div>Register</div>
+        </Grid>
+      </Grid>
+      <Grid item container justifyContent="center" alignItems="center">
+        <form onSubmit={formik.handleSubmit}>
+          <Grid
+            item
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            xs={10}
+            sm={8}
+            md={6}
+            lg={4}
+          >
+            <Grid
+              item
+              className="text-white font-normal text-base"
+              container
+              justifyContent="center"
+              xs={10}
+              sm={8}
+              md={6}
+              lg={4}
+            >
+              <Grid>
+                <div>Already have an account</div>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <FormInput
+                name="firstName"
+                formik={formik}
+                handleChange={formik.handleChange}
+                className="font-inter font-normal text-base"
+                label="First name"
+                placeholder="First name"
+                isHint={true}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <FormInput
+                name="lastName"
+                formik={formik}
+                handleChange={formik.handleChange}
+                className="font-inter font-normal text-base"
+                label="Last name"
+                placeholder="Last name"
+                isHint={true}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <FormInput
+                type="email"
+                name="email"
+                formik={formik}
+                handleChange={formik.handleChange}
+                className="font-inter font-normal text-base"
+                label="Email"
+                placeholder="Email"
+                isHint={true}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <FormInput
+                name="mobile"
+                formik={formik}
+                handleChange={formik.handleChange}
+                label="Mobile(optional)"
+                className="font-inter font-normal text-base"
+                placeholder="Mobile(optional)"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <FormCheck
+                type="checkbox"
+                name="acceptTerms"
+                className=""
+                label="I would like to receive communications
+              from the AFL and AFL partners about products and
+              initiatives of the AFL and AFL partners, including communications about AFL Mint pre-sales, new drops and
+              special offers. I agree to the terms and
+              conditions of the AFL Privacy Policy"
+                formik={formik}
+                handleChange={formik.handleChange}
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
     </Grid>
   )
 }
