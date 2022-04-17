02286782c4ba4db32220500ca3f64d84ead93e80
shinysource <highly.flores@gmail.com>
2022-04-17 08:41:43 -0700

    Added Thanks

---
+++ src/pages/main/Thanks.tsx
@@ -0,0 +1,40 @@
+import { Grid } from '@mui/material'
+import Footer from 'layout/Footer'
+
+const Thanks = () => {
+  return (
+    <Grid container className="img-back pt-9">
+      <Grid item container justifyContent="center">
+        <Grid item>
+          <img src="public/assets/logo/Logo.svg"></img>
+        </Grid>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="flex-start"
+          className="text-center"
+        >
+          <Grid item xs={9} sm={12} pt={4}>
+            <p className="font-podium49 text-[32px] font-bold uppercase">
+              Thanks, we’ve got your details
+            </p>
+          </Grid>
+          <Grid item xs={8} sm={7} md={5} lg={4} xl={3} pt={2}>
+            <p className="text-base font-normal">
+              We'll be in touch soon about the first drop. It's a game changer.
+              It's mint.
+            </p>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container alignItems="flex-end">
+        <Grid item sx={{ width: '100%' }}>
+          <Footer />
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Thanks
